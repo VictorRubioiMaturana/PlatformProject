9e84413bf128172802152468568817d72f123655
VictorR <victor.rubio@lcibarcelona.com>
2025-10-21 12:17:10 +0200

    Cambios básicos Sonido + Tilemap
    Cambiar el escenario de Tilemaps y subir lso archivos de los sonidos básicos.

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    [Header("Audio Sources")]
+    [SerializeField] AudioSource musicSource;
+    [SerializeField] AudioSource vfxSource;
+
+    [Header("Audio Clips")]
+    public AudioClip backgroundMusic;
+    public AudioClip jumpSound;
+
+
+
+
+}
